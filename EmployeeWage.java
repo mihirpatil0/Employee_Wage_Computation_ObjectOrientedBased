90c9db4d59cb7c234599c60879e45a39136f62d2
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-06 20:54:14 +0530

    Display: Welcome message.

---
+++ EmployeeWage.java
@@ -0,0 +1,23 @@
+package empwageobjectbased;
+
+public class EmployeeWage
+{
+	/**
+	 * Displaying Welcome Message.
+	 */
+	private void welcomeMessage() 
+	{
+		System.out.println("Welcome to Employee Wage Computation Problem .");
+	}
+	
+	/**
+	 * @param args
+	 * Creating objects of a class.
+	 * Calling different methods with objects if not static. 
+	 */
+	public static void main(String[] args)
+	{
+		EmployeeWage employeeWageObj = new EmployeeWage();
+		employeeWageObj.welcomeMessage();
+	}
+}
